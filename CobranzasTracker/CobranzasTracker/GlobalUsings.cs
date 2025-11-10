c8566bbd286163b88334302a6c117c30713741fe
Daniel Estrada <danielesmar@gmail.com>
2025-11-10 09:49:11 -0600

    comprobantes

--- CobranzasTracker/CobranzasTracker/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using CobranzasTracker.Services;
-global using CobranzasTracker.Core.Interfaces;
-global using CobranzasTracker.Core.ViewModel;
-global using CobranzasTracker.Infrastructure.Services;
-global using CobranzasTracker.Core.Models;
-global using Fonts;
--- CobranzasTracker/CobranzasTracker/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CobranzasTracker")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CobranzasTracker.Views")]
