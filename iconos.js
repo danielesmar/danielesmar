7d24127a976d290c85e659fba6bf24755fdae7d3
Daniel Estrada <danielesmar@gmail.com>
2025-11-21 15:02:21 -0600

    iconos dinamicos

---
+++ iconos.js
@@ -0,0 +1,8 @@
+[
+  "img/House - casa hogar vivienda inicio principal.svg",
+  "img/negative/House - casa hogar vivienda inicio principal_negative.svg",
+  "img/User - perfil usuario cliente login.svg",
+  "img/negative/User - perfil usuario cliente login_negative.svg",
+  "img/Settings - ajustes configuracion opciones.svg",
+  "img/Alert - advertencia peligro error fallo.svg"
+]
