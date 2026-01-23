1620409362f3c0969afbd14c8ba59e3f93f00c0e
Daniel Estrada <danielesmar@gmail.com>
2026-01-23 17:36:59 -0600

    feat: detalle cliente

--- azure-gpt/config.js
+++
@@ -1,7 +0,0 @@
-
-export const config = {
-  organization: "",
-  project: "",
-  baseUrl: "https://dev.azure.com",
-  pat: ""
-};
--- iconos.js
+++
@@ -1,8 +0,0 @@
-[
-  "img/House - casa hogar vivienda inicio principal.svg",
-  "img/negative/House - casa hogar vivienda inicio principal_negative.svg",
-  "img/User - perfil usuario cliente login.svg",
-  "img/negative/User - perfil usuario cliente login_negative.svg",
-  "img/Settings - ajustes configuracion opciones.svg",
-  "img/Alert - advertencia peligro error fallo.svg"
-]
